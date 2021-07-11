073fe1bebe0284956c1d30f98221a61ef0cbdf5f
Akio Orihara <akio.orihara@outlook.com>
2021-07-11 08:46:49 -0500

    completed it

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,71 @@ namespace VowelCounter
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(238, 216);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(235, 178);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter a Word";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(354, 178);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(73, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Count Y? ";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(238, 256);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 22);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Count Vowels";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,44 @@ namespace VowelCounter
         {
             InitializeComponent();
         }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int count_v = 0;
+            int count_y = 0;
+
+            string words = textBox1.Text;
+            for (int i = 0; i < words.Length; i++)
+            {
+                //string c = words[i].ToString().ToLower(); //Not sure why above line does need ToString() 
+                char c = char.ToLower(words[i]);  
+                if ("aiueo".Contains(c))
+                {
+                    count_v += 1;     
+                }
+                if (checkBox1.Checked) {
+                    if("y".Contains(c))
+                        count_y += 1;
+                }
+
+            }
+            if (count_v > 0)
+                MessageBox.Show("You entered : " + words + "\n" + "Your total vowel is " + count_v);
+            if (count_y > 0)
+                MessageBox.Show("Y found found : " + count_y);
+            else
+            {
+                MessageBox.Show("No Vowels Found");
+            }
+
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
